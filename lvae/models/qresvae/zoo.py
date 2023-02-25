8a5f94cde6e817ea95ce80a6a5663a55eda2fbd8
Zhihao Duan <duan90@purdue.edu>
2023-02-24 19:13:25 -0500

    add pretrained weights for qresvae

--- lvae/models/qresvae/zoo.py
+++ lvae/models/qresvae/zoo.py
@@ -1,6 +1,8 @@
-import lvae.models.qresvae.model as qres
+import torch
+from torch.hub import load_state_dict_from_url
 
 from lvae.models.registry import register_model
+import lvae.models.qresvae.model as qres
 
 
 @register_model
@@ -45,12 +47,21 @@ def qres34m(lmb=32, pretrained=False):
     cfg['max_stride'] = 64
 
     model = qres.HierarchicalVAE(cfg)
+    if (pretrained is True) and (lmb in {32, 64, 128, 256, 512, 1024, 2048}):
+        url = f'https://huggingface.co/duanzh0/my-model-weights/resolve/main/qres34m/lmb{lmb}/last_ema.pt'
+        msd = load_state_dict_from_url(url)['model']
+        model.load_state_dict(msd)
+    elif isinstance(pretrained, str):
+        msd = torch.load(pretrained)['model']
+        model.load_state_dict(msd)
+    else:
+        assert pretrained is False, f'Invalid {pretrained=} and {lmb=}'
     return model
 
 
 @register_model
-def qres34m_lossless():
-    cfg = SimpleConfig()
+def qres34m_lossless(pretrained=False):
+    cfg = dict()
 
     enc_nums = [6, 6, 6, 4, 2]
     dec_nums = [1, 2, 3, 3, 3]
@@ -91,12 +102,21 @@ def qres34m_lossless():
     cfg['max_stride'] = 64
 
     model = qres.HierarchicalVAE(cfg)
+    if pretrained is True:
+        url = 'https://huggingface.co/duanzh0/my-model-weights/resolve/main/qres34m/lossless/last_ema.pt'
+        msd = load_state_dict_from_url(url)['model']
+        model.load_state_dict(msd)
+    elif isinstance(pretrained, str):
+        msd = torch.load(pretrained)['model']
+        model.load_state_dict(msd)
+    else:
+        assert pretrained is False, f'Invalid {pretrained=}'
     return model
 
 
 @register_model
-def qres17m(lmb=None):
-    cfg = SimpleConfig()
+def qres17m(lmb=8, pretrained=True):
+    cfg = dict()
 
     enc_nums = [6,6,4,2]
     dec_nums = [1,2,4,5]
@@ -133,4 +153,13 @@ def qres17m(lmb=None):
     cfg['max_stride'] = 64
 
     model = qres.HierarchicalVAE(cfg)
+    if (pretrained is True) and (lmb in {1, 2, 4, 8, 16, 32, 64}):
+        url = f'https://huggingface.co/duanzh0/my-model-weights/resolve/main/qres17m/lmb{lmb}/last_ema.pt'
+        msd = load_state_dict_from_url(url)['model']
+        model.load_state_dict(msd)
+    elif isinstance(pretrained, str):
+        msd = torch.load(pretrained)['model']
+        model.load_state_dict(msd)
+    else:
+        assert pretrained is False, f'Invalid {pretrained=} and {lmb=}'
     return model
