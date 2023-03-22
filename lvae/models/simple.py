845e63331256f8d45d1769e3b72aebcd87093c06
Zhihao Duan <duan90@purdue.edu>
2023-03-22 09:01:55 -0400

    ae eval bugfix

--- lvae/models/simple.py
+++ lvae/models/simple.py
@@ -10,7 +10,7 @@ from lvae.models.registry import register_model
 
 
 class AutoEncoder(nn.Module):
-    def __init__(self, N=128, M=64):
+    def __init__(self, N=128, M=128):
         super().__init__()
 
         self.g_a = nn.Sequential(
@@ -44,11 +44,34 @@ class AutoEncoder(nn.Module):
         stats['loss'] = loss
 
         # ================ Logging ================
-        stats['bppix'] = math.prod(y.shape[1:4]) * 32 / math.prod(x.shape[2:4])
+        stats['bpp'] = math.prod(y.shape[1:4]) * 32 / math.prod(x.shape[2:4])
         psnr = -10 * math.log10(loss.item())
         stats['psnr'] = psnr
 
         return stats
+
+    def self_evaluate(self, img_dir, log_dir=None):
+        from pathlib import Path
+        from tqdm import tqdm
+        from PIL import Image
+        from collections import defaultdict
+        import torchvision.transforms.functional as tvf
+        img_paths = list(Path(img_dir).rglob('*.*'))
+        pbar = tqdm(img_paths, position=0, ascii=True)
+        all_image_stats = defaultdict(float)
+        for impath in pbar:
+            img = Image.open(impath)
+            im = tvf.to_tensor(img).unsqueeze_(0).to(device=self._dummy.device)
+            stats = self.forward(im)
+            # accumulate results
+            all_image_stats['count'] += 1
+            all_image_stats['loss']  += stats['loss']
+            all_image_stats['bpp']   += stats['bpp']
+            all_image_stats['psnr']  += stats['psnr']
+        # average over all images
+        count = all_image_stats.pop('count')
+        avg_stats = {k: v/count for k,v in all_image_stats.items()}
+        return avg_stats
 
 
 @register_model
