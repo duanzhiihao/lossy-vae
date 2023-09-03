3d4722c884fd1dff94722790a8ec45b03f0e3e28
Zhihao Duan <duan90@purdue.edu>
2023-09-03 12:22:25 -0400

    implement fdict

--- lvae/models/qarv/model.py
+++ lvae/models/qarv/model.py
@@ -200,8 +200,8 @@ class VariableRateLossyVAE(nn.Module):
         x = im.clone().add_(-0.5).mul_(2.0)
         return x
 
-    @torch.no_grad()
-    def _forward_flops(self, im, lmb):
+    @torch.inference_mode()
+    def _forward_flops(self, im):
         im = im.uniform_(0, 1)
         if self._flops_mode == 'compress':
             compressed_obj = self.compress(im)
@@ -209,6 +209,7 @@ class VariableRateLossyVAE(nn.Module):
             n, h, w = im.shape[0], im.shape[2]//self.max_stride, im.shape[3]//self.max_stride
             samples = self.unconditional_sample(bhw_repeat=(n,h,w))
         elif self._flops_mode == 'end-to-end':
+            lmb = self.sample_lmb(n=im.shape[0])
             x_hat, stats_all = self.forward_end2end(im, lmb=lmb)
         else:
             raise ValueError(f'Unknown self._flops_mode: {self._flops_mode}')
@@ -220,6 +221,7 @@ class VariableRateLossyVAE(nn.Module):
         low, high = math.pow(low, 1/p), math.pow(high, 1/p) # transformed space
         transformed_lmb = low + (high-low) * torch.rand(n, device=self._dummy.device)
         lmb = torch.pow(transformed_lmb, exponent=p)
+        assert isinstance(lmb, torch.Tensor) and lmb.shape == (n,)
         return lmb
 
     def expand_to_tensor(self, input_, n):
@@ -273,59 +275,54 @@ class VariableRateLossyVAE(nn.Module):
                 return fdict
             else:
                 fdict['feature'] = block(fdict['feature'])
+        fdict['x_hat'] = fdict.pop('feature') # rename 'feature' to 'x_hat'
         return fdict
 
-    def forward(self, im, lmb=None, return_rec=False):
+    def forward(self, im, lmb=None, return_fdict=False):
         im = im.to(self._dummy.device)
-        nB, imC, imH, imW = im.shape # batch, channel, height, width
+        B, imC, imH, imW = im.shape # batch, channel, height, width
 
         # ================ computing flops ================
         if self._flops_mode:
-            lmb = self.sample_lmb(n=im.shape[0])
-            return self._forward_flops(im, lmb)
+            return self._forward_flops(im)
 
         # ================ Forward pass ================
-        if (lmb is None): # training
-            lmb = self.sample_lmb(n=im.shape[0])
-        assert isinstance(lmb, torch.Tensor) and lmb.shape == (nB,)
-        raise NotImplementedError()
-        x_hat, stats_all = self.forward_end2end(im, lmb)
+        lmb = self.sample_lmb(n=im.shape[0]) # (B,)
+        fdict = self.forward_end2end(im, lmb)
 
         # ================ Compute Loss ================
+        x_hat, kl_divs = fdict['x_hat'], fdict['kl_divs']
         # rate
-        kl_divergences = [stat['kl'].sum(dim=(1, 2, 3)) for stat in stats_all]
-        ndims = float(imC * imH * imW)
-        kl = sum(kl_divergences) / ndims # nats per dimension
+        kl_divs = [kl.sum(dim=(1, 2, 3)) for kl in kl_divs]
+        kl = sum(kl_divs) / float(imC * imH * imW) # nats per dimensionm, shape (B,)
         # distortion
         x_target = self.preprocess_target(im)
-        distortion = self.distortion_func(x_hat, x_target)
+        distortion = self.distortion_func(x_hat, x_target) # (B,)
         # rate + distortion
-        loss = kl + lmb * distortion
-        loss = loss.mean(0)
+        loss = kl + lmb * distortion # (B,)
 
         metrics = OrderedDict()
-        metrics['loss'] = loss
+        metrics['loss'] = loss.mean(0)
 
         # ================ Logging ================
-        with torch.no_grad():
-            # for training print
-            metrics['bppix'] = kl.mean(0).item() * self.log2_e * imC
+        with torch.inference_mode(): # for training progress bar
+            metrics['bpp'] = kl.mean(0).item() * self.log2_e * imC
             metrics[self.distortion_name] = distortion.mean(0).item()
             im_hat = self.process_output(x_hat.detach())
             im_mse = tnf.mse_loss(im_hat, im, reduction='mean')
             psnr = -10 * math.log10(im_mse.item())
             metrics['psnr'] = psnr
-
-        if return_rec:
-            metrics['im_hat'] = im_hat
+        if return_fdict:
+            return metrics, fdict
         return metrics
 
+    @torch.inference_mode()
     def conditional_sample(self, lmb, latents, emb=None, bhw_repeat=None, t=1.0):
         """ sampling, conditioned on a list of latents variables
 
         Args:
             latents (torch.Tensor): latent variables. If None, do unconditional sampling
-            bhw_repeat (tuple): the constant bias will be repeated (batch, height, width) times
+            bhw_repeat (tuple): (batch, height, width) for the initial top-down feature
             t (float): temprature
         """
         if latents[0] is None:
@@ -338,21 +335,30 @@ class VariableRateLossyVAE(nn.Module):
         lmb = self.expand_to_tensor(lmb, n=nB)
         if emb is None:
             emb = self._get_lmb_embedding(lmb, n=nB)
+
+        fdict = dict() # a feature dictionary containing all features
+        fdict['lmb_emb'] = emb
+        fdict['dec_features'] = [] # top-down decoder features
+        fdict['zs'] = [] # latent variables
+        fdict['kl_divs'] = [] # kl (i.e., rate) for each latent variable
+        fdict['bit_strings'] = [] # compressed bit strings; only used in 'compress' mode
         feature = self.get_bias(bhw_repeat=(nB, nH, nW))
+        fdict['feature'] = feature # main feature; will be updated in the following loop
+
         idx = 0
         for i, block in enumerate(self.dec_blocks):
             if getattr(block, 'is_latent_block', False):
-                raise NotImplementedError()
-                feature, _ = block(feature, emb, mode='sampling', latent=latents[idx], t=t)
+                fdict = block(fdict, mode='sampling', latent=latents[idx], t=t)
                 idx += 1
             elif getattr(block, 'requires_embedding', False):
-                feature = block(feature, emb)
+                fdict['feature'] = block(fdict['feature'], emb)
             else:
-                feature = block(feature)
+                fdict['feature'] = block(fdict['feature'])
         assert idx == len(latents)
-        im_samples = self.process_output(feature)
+        im_samples = self.process_output(fdict['feature'])
         return im_samples
 
+    @torch.inference_mode()
     def unconditional_sample(self, lmb, bhw_repeat, t=1.0):
         """ unconditionally sample, ie, generate new images
 
@@ -362,14 +368,14 @@ class VariableRateLossyVAE(nn.Module):
         """
         return self.conditional_sample(lmb, [None]*self.num_latents, bhw_repeat=bhw_repeat, t=t)
 
-    @torch.no_grad()
+    @torch.inference_mode()
     def study(self, save_dir, **kwargs):
         save_dir = Path(save_dir)
         if not save_dir.is_dir():
             save_dir.mkdir(parents=False)
 
         lmb = self.expand_to_tensor(self.default_lmb, n=1)
-        # unconditional samples
+        # unconditional sampling
         for k in [1, 2]:
             num = 6
             im_samples = self.unconditional_sample(lmb, bhw_repeat=(num,k,k))
@@ -379,31 +385,28 @@ class VariableRateLossyVAE(nn.Module):
         for imname in self._logging_images:
             impath = f'images/{imname}'
             im = tvf.to_tensor(Image.open(impath)).unsqueeze_(0).to(device=self._dummy.device)
-            raise NotImplementedError()
-            x_hat, _ = self.forward_end2end(im, lmb=lmb)
-            im_hat = self.process_output(x_hat)
+            fdict = self.forward_end2end(im, lmb=lmb)
+            im_hat = self.process_output(fdict['x_hat'])
             tv.utils.save_image(torch.cat([im, im_hat], dim=0), fp=save_dir / imname)
 
-    @torch.no_grad()
+    @torch.inference_mode()
     def _self_evaluate(self, img_paths, lmb: float, pbar=False, log_dir=None):
         pbar = tqdm(img_paths) if pbar else img_paths
-        all_image_stats = defaultdict(float)
-        # self._stats_log = dict()
+        all_image_stats = defaultdict(AverageMeter)
         if log_dir is not None:
             log_dir = Path(log_dir)
             channel_bpp_stats = defaultdict(AverageMeter)
         for impath in pbar:
             img = Image.open(impath)
             imgh, imgw = img.height, img.width
-            # img = coding.crop_divisible_by(img, div=self.max_stride)
             img_padded = coding.pad_divisible_by(img, div=self.max_stride)
             im = tvf.to_tensor(img_padded).unsqueeze_(0).to(device=self._dummy.device)
-            raise NotImplementedError()
-            x_hat, stats_all = self.forward_end2end(im, lmb=self.expand_to_tensor(lmb,n=1))
-            x_hat = x_hat[:, :, :imgh, :imgw]
+
+            fdict = self.forward_end2end(im, lmb=self.expand_to_tensor(lmb,n=1))
+            x_hat = fdict['x_hat'][:, :, :imgh, :imgw]
             # compute bpp
             _, imC, imH, imW = im.shape
-            kl = sum([stat['kl'].sum(dim=(1, 2, 3)) for stat in stats_all]).mean(0) / (imC*imgh*imgw)
+            kl = sum([kl.sum(dim=(1, 2, 3)) for kl in fdict['kl_divs']]).mean(0) / (imC*imgh*imgw)
             bpp_estimated = kl.item() * self.log2_e * imC
             # compute psnr
             im = tvf.to_tensor(img).unsqueeze_(0).to(device=self._dummy.device)
@@ -414,19 +417,17 @@ class VariableRateLossyVAE(nn.Module):
             mse = tnf.mse_loss(real, fake, reduction='mean').item()
             psnr = float(-10 * math.log10(mse))
             # accumulate results
-            all_image_stats['count'] += 1
-            all_image_stats['loss'] += float(kl.item() + lmb * distortion)
-            all_image_stats['bpp']  += bpp_estimated
-            all_image_stats['psnr'] += psnr
+            all_image_stats['loss'].update(float(kl.item() + lmb * distortion))
+            all_image_stats['bpp'].update(bpp_estimated)
+            all_image_stats['psnr'].update(psnr)
             # debugging
             if log_dir is not None:
                 _to_bpp = lambda kl: kl.sum(dim=(2,3)).mean(0).cpu() / (imH*imW) * self.log2_e
-                channel_bpps = [_to_bpp(stat['kl']) for stat in stats_all]
+                channel_bpps = [_to_bpp(kl) for kl in fdict['kl_divs']]
                 for i, ch_bpp in enumerate(channel_bpps):
                     channel_bpp_stats[i].update(ch_bpp)
         # average over all images
-        count = all_image_stats.pop('count')
-        avg_stats = {k: v/count for k,v in all_image_stats.items()}
+        avg_stats = {k: v.avg for k,v in all_image_stats.items()}
         avg_stats['lambda'] = lmb
         if log_dir is not None:
             self._log_channel_stats(channel_bpp_stats, log_dir, lmb)
@@ -477,11 +478,10 @@ class VariableRateLossyVAE(nn.Module):
     @torch.no_grad()
     def compress(self, im, lmb=None):
         lmb = lmb or self.default_lmb # if no lmb is provided, use the default one
-        raise NotImplementedError()
-        lv_block_results = self.forward_end2end(im, lmb=lmb, mode='compress')
-        assert len(lv_block_results) == self.num_latents
+        fdict = self.forward_end2end(im, lmb=lmb, mode='compress')
+        assert len(fdict['bit_strings']) == self.num_latents
         assert im.shape[0] == 1, f'Right now only support a single image, got {im.shape=}'
-        all_lv_strings = [res['strings'][0] for res in lv_block_results]
+        all_lv_strings = [strings[0] for strings in fdict['bit_strings']]
         string = coding.pack_byte_strings(all_lv_strings)
         # encode lambda and image shape in the header
         nB, _, imH, imW = im.shape
@@ -500,23 +500,28 @@ class VariableRateLossyVAE(nn.Module):
         (nB, nH, nW), string = struct.unpack('3H', string[:_len]), string[_len:]
         all_lv_strings = coding.unpack_byte_string(string)
 
+        fdict = dict() # a feature dictionary containing all features
         lmb = self.expand_to_tensor(lmb, n=nB)
-        lmb_embedding = self._get_lmb_embedding(lmb, n=nB)
-
+        fdict['lmb_emb'] = self._get_lmb_embedding(lmb, n=nB)
+        fdict['dec_features'] = [] # top-down decoder features
+        fdict['zs'] = [] # latent variables
+        fdict['kl_divs'] = [] # kl (i.e., rate) for each latent variable
+        fdict['bit_strings'] = [] # compressed bit strings; only used in 'compress' mode
         feature = self.get_bias(bhw_repeat=(nB, nH, nW))
+        fdict['feature'] = feature # main feature; will be updated in the following loop
+
         str_i = 0
         for bi, block in enumerate(self.dec_blocks):
             if getattr(block, 'is_latent_block', False):
                 strs_batch = [all_lv_strings[str_i],]
-                raise NotImplementedError()
-                feature, _ = block(feature, lmb_embedding, mode='decompress', strings=strs_batch)
+                fdict = block(fdict, mode='decompress', strings=strs_batch)
                 str_i += 1
             elif getattr(block, 'requires_embedding', False):
-                feature = block(feature, lmb_embedding)
+                fdict['feature'] = block(fdict['feature'], fdict['lmb_emb'])
             else:
-                feature = block(feature)
+                fdict['feature'] = block(fdict['feature'])
         assert str_i == len(all_lv_strings), f'str_i={str_i}, len={len(all_lv_strings)}'
-        im_hat = self.process_output(feature)
+        im_hat = self.process_output(fdict['feature'])
         return im_hat
 
     @torch.no_grad()
